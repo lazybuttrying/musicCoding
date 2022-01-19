a41bee7514a3a9037468ec47edf26ba4b43ee48b
lazybuttrying <finallazy9999@gmail.com>
2022-01-19 19:05:00 +0900

    Initial Commit

---
+++ musicCoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  musicCoding
+//
+//  Created by apple on 2022/01/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
